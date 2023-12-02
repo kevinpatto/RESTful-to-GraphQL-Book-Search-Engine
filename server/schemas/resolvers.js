5cb9ceca60eda778ac5488e978c320735d20c807
Kevin Patto <kevp2010@gmail.com>
2023-12-01 22:40:01 -0600

    unfinished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,19 +1,61 @@
 const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    getSingleUser: async (parent, { user = null, params }) => {
+    getSingleUser: async (parent, { username }) => {
       return await User.findOne({
         $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
       });
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
   },
 
   Mutation: {
-    createUser: async ({ body }, res) => {
-      const user = await User.create(body);
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { user, bookInput }) => {
+      return await User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: { bookInput } } },
+        { new: true, runValidators: true }
+      );
+    },
+    // remove a book from `savedBooks`
+    deleteBook: async ({ user, params }, res) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "Couldn't find user with this id!" });
+      }
+      return res.json(updatedUser);
     },
   }
 };
