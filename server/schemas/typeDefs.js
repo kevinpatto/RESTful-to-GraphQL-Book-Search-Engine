fdfc5fe6b8b870bb5d0bc3659c245c48e0e60a97
Kevin Patto <kevp2010@gmail.com>
2023-12-01 22:39:23 -0600

    added typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,32 @@
 const typeDefs = `
-  
+  type User {
+	_id: ID
+	username: String
+	email: String
+	password: String
+	savedBooks: [String]
+  }
+
+  type Auth {
+	token: ID!
+	user: User
+  }
+
+  input BookInput {
+	
+  }
+
+  type Query {
+	getSingleUser(userId: ID!): User
+	me: Profile
+  }
+
+  type Mutation {
+	createUser(username: String!, email: String!, password: String!): Auth
+	login(email: String!, password: String!): Auth
+	saveBook(userId: ID!, bookId: String!): User
+	deleteBook(bookId: String!): User
+  }
 `;
 
 module.exports = typeDefs;
