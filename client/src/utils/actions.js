f97c9efbe3f4f38510919a84be40ce6cecdd5a08
Kevin Patto <kevp2010@gmail.com>
2023-12-03 22:01:54 -0600

    created file

---
+++ client/src/utils/actions.js
@@ -0,0 +1,8 @@
+export const GET_SINGLE_USER = 'GET_SINGLE_USER';
+export const GET_ME = 'GET_ME'
+
+export const LOGIN = 'LOGIN';
+
+export const CREATE_USER = 'CREATE_USER';
+export const SAVE_BOOK = 'SAVE_BOOK';
+export const DELETE_BOOK = 'DELETE_BOOK';
