6ddadc5222148f6e717103c391a45d70a45679b5
Kevin Patto <kevp2010@gmail.com>
2023-12-03 22:02:41 -0600

    attempting to get apollogql to work

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 // const routes = require('./routes');
@@ -27,8 +29,14 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+
+  app.use('/images', express.static(path.join(__dirname, '../client/public/images')));
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
   
 // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
