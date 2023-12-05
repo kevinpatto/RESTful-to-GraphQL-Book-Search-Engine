eb3d728c7e5e22cb21fe592d254a98e02f77825e
Kevin Patto <kevp2010@gmail.com>
2023-12-04 19:13:29 -0600

    added working getSingleUser query and questionable Me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,25 @@
 import { gql } from '@apollo/client';
 
+export const GET_SINGLE_USER = gql`
+query Query($userId: ID!) {
+	getSingleUser(userId: $userId) {
+		_id
+		username
+		email
+		password
+		savedBooks
+	}
+}
+`
+
+export const GET_ME = gql`
+query Me {
+	me {
+		_id
+		username
+		email
+		password
+		savedBooks
+	}
+  }
+`;
